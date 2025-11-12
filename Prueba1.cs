adec92cfa004621a77afec2eaa6ac9cfe6bbe590
Emir Rios <emirrios201405@gmail.com>
2025-11-11 19:24:40 -0600

    Agregado archivo Prueba1.cs con lectura y escritura en consola

---
+++ Prueba1.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace CitasMedic
+{
+    class Prueba1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Henry");
+
+            Console.Write("Escribe tu apellido: ");
+            string apellido = Console.ReadLine();
+
+            Console.WriteLine("Tu apellido es: " + apellido);
+
+            Console.WriteLine("Mi nombre completo es: Henry " + apellido);
+
+            // Hola a todos //
+        }
+    }
+}
